1c6d07c1cfb7dac88c9d315aeafcb9850a9a5261
Thierry Chappuis <tchappui@users.noreply.github.com>
2020-09-09 06:24:43 +0200

    Update parsecleaner.py

--- food/parsecleaner.py
+++ food/parsecleaner.py
@@ -10,6 +10,9 @@ from settings import NBR_PRODUCT_PER_PAGE, param_categories, fields
 class Parser:
     """process the data retrieve from the API openfoodfact."""
 
+    # -tc- je passerais la liste des produits en paramètre de clean(),
+    # -tc- pas du constructeur. Et la liste nettoyée peut simplement
+    # -tc- être nettoyée par clean.
     def __init__(self, api_result):
         self.api_result = api_result
         self.products_list = []  # list of all the cleaned product
@@ -18,13 +21,21 @@ class Parser:
         """clean, filter and valid the data from the api and return a list of product ready to be
         upload to purebeurre database."""
 
+        # -tc- revoir le parcour des listes avec une boucle for
         for category in range(len(param_categories)):
+            # -tc- Est-ce vraiment important ce nombre de produits? Mettons tout
+            # -tc- ce qu'on peut.
             for num_product_per_category in range(NBR_PRODUCT_PER_PAGE):
                 product = {}  # initialisation of a product as a dict
                 product_completeness = True
+                # -tc- for field in fields
                 for i in range(len(fields)):
                     try:
                         # test if content
+                        # -tc- utilise directement la méthode get de ton dictionnaire
+                        # -tc- ce qui évite la KeyError
+
+                        # -tc- personnellement, je créerais une méthode de validation séparée de clean().
                         if (self.api_result[category]['products']
                                 [num_product_per_category]
                                 [fields[i]]) != "":
@@ -32,8 +43,9 @@ class Parser:
                             result = (
                                 self.api_result[category]['products']
                                 [num_product_per_category][fields[i]]).strip()
-                            if isinstance(result, str):  # if result == str
+                            if isinstance(result, str):  # if result == str -tc- commrntaire inutile
                                 # regex used to remove unwanted characters
+                                # -tc- utilise une raw string pour tes expressions rationnelles
                                 result = re.sub(
                                     "[.,;\t\n\r\x0b\x0c]", '', result)
                                 product.update({fields[i]: result})
